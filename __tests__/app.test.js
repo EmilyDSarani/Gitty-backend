a33b2d0b9ad9f88212189352b0281ffedfbfe0fa
EmilyDSarani <emily.sarani@gmail.com>
2022-02-11 16:53:11 -0600

    test written for create

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,10 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend routes', () => {
+jest.mock('../lib/utils/github');
+
+
+describe('Gitty Routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -11,4 +14,17 @@ describe('backend routes', () => {
   afterAll(() => {
     pool.end();
   });
+  it('should redirect to github oauth on login', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback&scope=user/i
+    );
+  });
+
+
+
+
+
+
 });
